e98be8f8a664ad68777441182a5ad9cfae9761ba
javi <falvid16@gmail.com>
2023-03-03 20:15:31 -0700

    Minor layout changes

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export default function ProjectCard() {
+    return (
+        <div className="bg-slate-900 w-full h-72 rounded my-5 md:mx-5 md:w-60">
+            1
+        </div>
+    )
+}
